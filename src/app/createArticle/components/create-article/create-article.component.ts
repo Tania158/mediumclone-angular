73263bbc576b36831f0d7e8adef41a4f6c49a30f
Tetiana Pavlova <pavlovatania97@gmail.com>
2023-01-26 21:36:33 +0200

    post creation form

--- src/app/createArticle/components/create-article/create-article.component.ts
+++ src/app/createArticle/components/create-article/create-article.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { select, Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+import { AppStateInterface } from 'src/app/shared/types/appState.interface';
+import { ArticleInputInterface } from 'src/app/shared/types/articleInput.interface';
+import { BackendErrorsInterface } from 'src/app/shared/types/backendErrors.interface';
 
 @Component({
   selector: 'app-create-article',
   templateUrl: './create-article.component.html',
   styleUrls: ['./create-article.component.scss']
 })
-export class CreateArticleComponent {
+export class CreateArticleComponent implements OnInit {
+  initialValues: ArticleInputInterface = {
+    title: '',
+    description: '',
+    body: '',
+    tagList: []
+  }
+  isSubmitting$!: Observable<boolean>
+  backendErrors$!: Observable<BackendErrorsInterface | null>
 
+  constructor(private store: Store<AppStateInterface>) {}
+
+  ngOnInit(): void {
+    // this.isSubmitting$ = this.store.pipe(select(isSubmittingSelector))
+    // this.backendErrors$ = this.store.pipe(select(validationErrorsSelector))
+  }
+
+  onSubmit(articleInput: ArticleInputInterface): void {
+    console.log(articleInput)
+    // this.store.dispatch(createArticleAction({articleInput}))
+  }
 }
+// function createArticleAction(arg0: { articleInput: ArticleInputInterface; }): any {
+//   throw new Error('Function not implemented.');
+// }
+
--- src/app/shared/modules/articleForm/articleForm.module.ts
+++ src/app/shared/modules/articleForm/articleForm.module.ts
@@ -1,9 +1,15 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from '@angular/common';
 import { ArticleFormComponent } from './components/article-form/article-form.component';
+import { ReactiveFormsModule } from '@angular/forms';
+import { BackendErrorMessagesModule } from '../backendErrorMessages/backendErrorMessages.module';
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    BackendErrorMessagesModule
+  ],
   declarations: [
     ArticleFormComponent
   ],
