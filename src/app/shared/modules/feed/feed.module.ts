0a478248885850f99e82b7f315e6ded5aada13ff
Tetiana Pavlova <pavlovatania97@gmail.com>
2023-01-29 15:39:55 +0200

    added addToFavorites component

--- src/app/shared/modules/feed/feed.module.ts
+++ src/app/shared/modules/feed/feed.module.ts
@@ -10,6 +10,7 @@ import { RouterModule } from '@angular/router';
 import { ErrorMessageModule } from '../errorMessage/errorMessage.module';
 import { LoadingModule } from '../loading/loading.module';import { PaginationModule } from '../pagination/pagination.module';
 import { TagListModule } from '../tagList/tagList.module';
+import { AddToFavoritesModule } from '../addToFavorites/addToFavorites.module';
 
 @NgModule({
   imports: [
@@ -20,7 +21,8 @@ import { TagListModule } from '../tagList/tagList.module';
     ErrorMessageModule,
     LoadingModule,
     PaginationModule,
-    TagListModule
+    TagListModule,
+    AddToFavoritesModule
   ],
   declarations: [FeedComponent],
   exports: [FeedComponent],
